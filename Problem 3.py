1eae3e3c2acfff2f483427c5e87935e9654880d5
Beatriz Rodrigues <anedok@outlook.com>
2026-04-23 00:19:52 -0300

    day 2

---
+++ Problem 3.py
@@ -0,0 +1,20 @@
+#The prime factors of 13195 are 5, 7, 13 and 29.
+#What is the largest prime factor of the number 600851475143?
+n = 600851475143
+prime = 0
+i = 3
+
+while n % 2 == 0:
+    prime = 2
+    n //= 2
+
+while i*i <= n:
+    while n % i == 0:
+        prime = i
+        n //= i 
+    i += 2
+
+if n > 1:
+    prime = n
+    
+print(prime)
